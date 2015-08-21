8404fbbd63298baeb7b79d86d757c9bc0ed4ce19
enidchinchilla <ed.chinchillac@gmail.com>
2015-08-21 01:46:46 -0600

    Another commit

--- Laboratorio2.Website/Laboratorio2.WCFLayer/EmployeeService.cs
+++ Laboratorio2.Website/Laboratorio2.WCFLayer/EmployeeService.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using Laboratorio2.BizLayer;
+using Laboratorio2.DataAccess;
+using System.Collections.Generic;
 
 namespace Laboratorio2.WCFLayer
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
     public class EmployeeService : IEmployeeService
     {
-        
+        public List<EmployeeDepartment> GetAll()
+        {
+            var employeeBL = new EmployeeBL();
+            return employeeBL.GetEmployeeDepartments(1);
+        }
     }
 }
--- Laboratorio2.Website/Laboratorio2.WCFLayer/IEmployeeService.cs
+++ Laboratorio2.Website/Laboratorio2.WCFLayer/IEmployeeService.cs
@@ -1,11 +1,14 @@
-﻿using System.ServiceModel;
+﻿using Laboratorio2.DataAccess;
+using System.Collections.Generic;
+using System.ServiceModel;
 
 namespace Laboratorio2.WCFLayer
 {
     [ServiceContract]
     public interface IEmployeeService
     {
-        
+        [OperationContract]
+        List<EmployeeDepartment> GetAll();
     }
 
 }
