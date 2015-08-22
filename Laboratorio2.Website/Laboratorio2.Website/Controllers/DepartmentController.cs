74323c7ddb351c1309a6418f707dad29d00718a8
enidchinchilla <ed.chinchillac@gmail.com>
2015-08-22 13:52:34 -0600

    Controllers added

---
+++ Laboratorio2.Website/Laboratorio2.Website/Controllers/DepartmentController.cs
@@ -0,0 +1,44 @@
+﻿using Laboratorio2.Website.EmployeeService;
+using Laboratorio2.Website.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Laboratorio2.Website.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(new DepartmentEmployeeModel());
+        }
+
+        [HttpPost]
+        public ActionResult Index(DepartmentEmployeeModel model)
+        {
+            var client = new EmployeeServiceClient();
+            var departments = client.GetDepartments(model.StartDate, model.EndDate, model.Name).ToList();
+            model.Items = departments;
+            return View(model);
+        }
+
+        public ActionResult Employee(string id)
+        {
+            var model = new DepartmentEmployeeModel();
+            if (!string.IsNullOrEmpty(id))
+            {
+                var departmentID = short.Parse(id);
+                if (departmentID > 0)
+                {
+                    var client = new EmployeeServiceClient();
+                    var departments = client.GetEmployeesByDepartmentID(departmentID).ToList();
+                    model.Items = departments;
+                }
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ Laboratorio2.Website/Laboratorio2.Website/Models/EmployeeDepartmentModel.cs
@@ -0,0 +1,16 @@
+﻿using Laboratorio2.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laboratorio2.Website.Models
+{
+    public class DepartmentEmployeeModel
+    {
+        public string Name { get; set; }
+        public Nullable<System.DateTime> StartDate { get; set; }
+        public Nullable<System.DateTime> EndDate { get; set; }
+        public List<EmployeeDepartment> Items { get; set; } 
+    }
+}
